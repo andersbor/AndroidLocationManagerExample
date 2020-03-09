6935616a2beb417a9797c7e543c8ae4f3a3daf0b
Anders Børjesson <anbo@zealand.dk>
2020-03-09 09:27:24 +0100

    updated to androidx

--- app/src/androidTest/java/dk/easj/anbo/locationmanagerexample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/dk/easj/anbo/locationmanagerexample/ExampleInstrumentedTest.java
@@ -1,8 +1,6 @@
 package dk.easj.anbo.locationmanagerexample;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,8 +17,6 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("dk.easj.anbo.locationmanagerexample", appContext.getPackageName());
     }
 }
